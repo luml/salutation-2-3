504b80436ae75edcfa7b62be988cce11f9234cad
Elio Lu <1520622848@qq.com>
2021-08-29 15:58:16 +0800

    feat: w**t he** is $attrs

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,20 @@
       v-model:salutation.capitalize="form.salutation"
       v-model:name.capitalize="form.name"
     ></SalutationName>
+    <BaseInput @blur="email = 'blurrr@its.cold'" class="thicc" type="email" v-model="email" label="Email:" />
   </div>
 </template>
 
 <script>
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 import SalutationName from "./components/SalutationName.vue";
+import BaseInput from "./components/BaseInput.vue";
 
 export default {
   name: "App",
   components: {
     SalutationName,
+    BaseInput,
   },
   setup() {
     const form = reactive({
@@ -22,8 +25,11 @@ export default {
       name: "",
     });
 
+    const email = ref("");
+
     return {
       form,
+      email,
     };
   },
 };
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <label for="">{{ label }}</label>
+    <input
+      v-bind="{
+        ...$attrs,
+        onIuput: (event) => $emit('update:modelValue', $event.target.value),
+      }"
+      type="email"
+      :value="modelValue"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    modelValue: {
+      type: [String, Number],
+      default: "",
+    },
+    label: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
